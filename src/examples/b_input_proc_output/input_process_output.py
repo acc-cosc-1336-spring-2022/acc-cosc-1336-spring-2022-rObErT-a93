d1022ea2428fc160d1f2388e9d6fd9175bc8b1c7
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-02-08 20:48:50 -0600

    Tues2/8_Lecture

--- src/examples/b_input_proc_output/input_process_output.py
+++ src/examples/b_input_proc_output/input_process_output.py
@@ -7,3 +7,24 @@ def test_config(): #value return function
 
 def add_numbers(num1, num2):
     return num1 + num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def integer_division(num1, num2):
+    return num1 // num2   
+
+def float_division(num1, num2):
+    return num1 / num2
+
+def operator_precedence_1(num1, num2, num3): # no paranthesis
+    return num1 + num2 / num3
+
+def operator_precedence_2(num1, num2, num3): # w/ paranthesis
+    return (num1 + num2) / num3
+
+def exponentiate_me(num1, num2):
+    return num1 ** num2
+
+def get_remainder(num1, num2):
+    return num1 % num2
--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -11,5 +11,11 @@ print(val0)
 
 num1 = int(input('enter num1: '))
 num2 = int(input('enter num2: '))
+num3 = int(input('enter num3: '))
 
-print(input_process_output.add_numbers(num1,num2))
+print(input_process_output.add_numbers(num1,num2)) #parameters give us flexibility/reuse
+print(input_process_output.multiply_numbers(num1, num2))
+print(input_process_output.integer_division(num1, num2))
+print(input_process_output.float_division(num1, num2))
+print(input_process_output.operator_precedence_2(num1, num2, num3))
+print(input_process_output.get_remainder(num1, num2))
