25d9fbc88d9c89782a7f51b05e53f0cd7ffd0560
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-02-16 12:38:12 -0600

    HW 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -4,11 +4,11 @@ def get_options_ratio(options, total):
 def get_faculty_rating(rating):
     if rating >= .9 and rating <= 1:
         return 'Excellent'
-    elif rating > .8:
+    elif rating >= .8:
         return 'Very Good'
-    elif rating > .7:
+    elif rating >= .7:
         return 'Good'
-    elif rating > .6:
+    elif rating >= .6:
         return 'Needs Improvement'
     else:
         rating >= 0 and rating <= .59
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -7,7 +7,7 @@ total = int(input('Enter 10 for TOTAL: '))
 result = decisions.get_options_ratio(options, total)
 if (options < 0.0 or options > 10.0):
     print('Invalid Entry, Try Again')
-elif (total > 10):
+elif (total != 10):
     print('Invalid Entry, Try Again')
 elif (result):
     print('Faculty Rating:', decisions.get_faculty_rating(result))
