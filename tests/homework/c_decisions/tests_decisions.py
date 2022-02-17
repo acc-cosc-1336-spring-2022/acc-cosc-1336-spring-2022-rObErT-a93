b4bcbafb65b5482b843d4fdf9c7faab992042648
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-02-16 20:14:57 -0600

    HW 2

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -8,9 +8,8 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(.5, get_options_ratio(10, 20))
 
     def test_get_faculty_rating(self):
-        self.assertEqual('Excellent', get_faculty_rating(get_options_ratio(9.1,10)))
-        self.assertEqual('Very Good', get_faculty_rating(get_options_ratio(8.5, 10)))
-        self.assertEqual('Good', get_faculty_rating(get_options_ratio(7.1, 10)))
-        self.assertEqual('Needs Improvement', get_faculty_rating(get_options_ratio(6.6, 10)))
-        self.assertEqual('Unacceptable', get_faculty_rating(get_options_ratio(5.9, 10)))
-        self.assertEqual('Unacceptable', get_faculty_rating(get_options_ratio(4.5, 10)))
+        self.assertEqual('Excellent', get_faculty_rating(.91))
+        self.assertEqual('Very Good', get_faculty_rating(.85))
+        self.assertEqual('Good', get_faculty_rating(.71))
+        self.assertEqual('Needs Improvement', get_faculty_rating(.66))
+        self.assertEqual('Unacceptable', get_faculty_rating(.45))
