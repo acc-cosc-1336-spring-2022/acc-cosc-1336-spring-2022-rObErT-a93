425a3430a31486ef52a467f5594abe10fe45d120
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-02-26 22:42:28 -0600

    HW 3

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,41 @@
-#
+import repetition
+
+print('Homework 3 Menu')
+def menu():
+    print('[1] Factorial')
+    print('[2] Sum of Odd Numbers')
+    print('[3] Exit')
+
+def prompt_user():
+    try_again = 'y'
+    while try_again != 'n':
+        try_again = input('Would you like to run program again?, y or n: ')
+
+menu()
+option = int(input('Enter an option: '))
+print()
+while option != 3:
+    if option == 1:
+        print()
+        print('[1]: Calculating Factorial')
+        num = int(input('Enter a value to calculate factorial: '))
+        result = repetition.get_factorial(num)
+        if num < 0 or num > 10:
+            print('Invalid entry {enter a value between 0 and 10}, Please try again!')
+        elif(result):
+            print('The Factorial = ', repetition.get_factorial(num))
+    elif option == 2:
+        print()
+        print('[2]: Sum of Odd Numbers')
+        num = int(input('Enter a value to calculate the sum of odd numbers: '))
+        result = repetition.sum_odd_numbers(num)
+        if num < 0 or num > 100:
+            print('Invalid entry {enter a value between 0 and 100}, Please try again!')
+        elif(result):  
+            print('The Sum of Odd Numbers = ', repetition.sum_odd_numbers(num))
+    else:
+        print('You have made in invalid selection.')
+    print()
+    print('Homework 3 Menu')
+    menu()
+    option = int(input('Enter an option: '))
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,14 @@
-#
+def get_factorial(num):
+    result = 1
+    for i in range(1,num+1):
+        result *= i
+    return result
+
+def sum_odd_numbers(num):
+    counter = 0
+    sum = 0
+    while (counter < num):
+        counter += 1
+        if counter % 2 == 1:
+            sum += counter
+    return sum
