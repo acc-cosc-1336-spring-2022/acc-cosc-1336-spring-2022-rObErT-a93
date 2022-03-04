8b8a313ce902db5356e661f07215971d6b4270fa
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-03-04 09:39:43 -0600

    3/4/2022

--- src/examples/h_strings/strings.py
+++ src/examples/h_strings/strings.py
@@ -1,2 +1,6 @@
 def test_config():
     return True
+
+def hello_world():
+    hello = 'Hello World!'
+    print(hello)
--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,42 +1,38 @@
 import repetition
 
-print('Homework 3 Menu')
 def menu():
+    print('Homework 3 Menu')
     print('[1] Factorial')
     print('[2] Sum of Odd Numbers')
     print('[3] Exit')
+    choice = int(input('Enter an option: '))
+    while choice != 3:
+        if choice == 1:
+            print()
+            print('[1]: Calculating Factorial')
+            num = int(input('Enter a value to calculate factorial: '))
+            result = repetition.get_factorial(num)
+            if num < 0 or num > 10:
+                print('Invalid entry {enter a value between 0 and 10}, Please try again!')
+            elif(result):
+                print('The Factorial = ', repetition.get_factorial(num))
+
+        elif choice == 2:
+            print()
+            print('[2]: Sum of Odd Numbers')
+            num = int(input('Enter a value to calculate the sum of odd numbers: '))
+            result = repetition.sum_odd_numbers(num)
+            if num < 0 or num > 100:
+                print('Invalid entry {enter a value between 0 and 100}, Please try again!')
+            elif(result):  
+                print('The Sum of Odd Numbers = ', repetition.sum_odd_numbers(num))
+
+        else:
+            print('You have made in invalid selection.')
 
 def prompt_user():
     try_again = 'y'
-    while try_again != 'n':
+    while try_again == 'y':
         try_again = input('Would you like to run program again?, y or n: ')
 
 menu()
-option = int(input('Enter an option: '))
-print()
-while option != 3:
-    if option == 1:
-        print()
-        print('[1]: Calculating Factorial')
-        num = int(input('Enter a value to calculate factorial: '))
-        result = repetition.get_factorial(num)
-        if num < 0 or num > 10:
-            print('Invalid entry {enter a value between 0 and 10}, Please try again!')
-        elif(result):
-            print('The Factorial = ', repetition.get_factorial(num))
-    elif option == 2:
-        print()
-        print('[2]: Sum of Odd Numbers')
-        num = int(input('Enter a value to calculate the sum of odd numbers: '))
-        result = repetition.sum_odd_numbers(num)
-        if num < 0 or num > 100:
-            print('Invalid entry {enter a value between 0 and 100}, Please try again!')
-        elif(result):  
-            print('The Sum of Odd Numbers = ', repetition.sum_odd_numbers(num))
-    else:
-        print('You have made in invalid selection.')
-    prompt_user()
-    print()
-    print('Homework 3 Menu')
-    menu()
-    option = int(input('Enter an option: '))
