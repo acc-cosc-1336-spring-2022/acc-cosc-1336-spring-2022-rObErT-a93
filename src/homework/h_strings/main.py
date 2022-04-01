0db4467390f65de69cb57a27ac55660b6741d8ce
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-04-01 01:28:10 -0500

    HW 5

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,46 @@
 #
+import strings
+
+def menu():
+    print('Main Menu')
+    print('[1] Hamming Distance')
+    print('[2] DNA Complement')
+    print('[3] Exit')
+    user_input = input('Choose an option: ')
+    if user_input == '1':
+        option_1()
+    elif user_input == '2':
+        option_2()
+    elif user_input == '3':
+        option_3()
+
+def option_1():
+    print('Enter a 16 character sequence of DNA,')
+    input_1 = input('Enter 1st DNA Sequence: ')
+    input_2 = input('Enter 2nd DNA Sequence: ')
+    result = strings.get_hamming_distance(input_1, input_2)
+    print('The Hamming Distance = ', result)
+    user_input = input('Would you like to continue again? y/n: ')
+    user_result = user_input
+    if user_result == 'n' or user_result == 'N':
+        menu()
+    if user_result == 'y' or user_result == 'Y':
+        option_1()
+
+def option_2():
+    print('Enter a 10 character sequence of DNA')
+    dna = input('Enter DNA sequence: ')
+    result = strings.get_dna_complement(dna)
+    print('DNA Complement = ', result)
+    user_input = input('Would you like to continue again? y/n: ')
+    user_result = user_input
+    if user_result == 'n' or user_result == 'N':
+        menu()
+    if user_result == 'y' or user_result == 'Y':
+        option_2()
+
+def option_3():
+    print('Thank you...Goodbye')
+    quit()
+
+menu()
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -3,10 +3,27 @@ def get_hamming_distance(dna1, dna2):
     list_dna2 = list(dna2)
     counter = 0
 
-    for i in range(17):
+    for i in range(16):
         if list_dna1[i] != list_dna2[i]:
             counter += 1
     return counter
+
+def get_dna_complement(dna):
+    reverse = reverse_string(dna)
+    new_string = list(reverse)
+    for i in range(10):
+        if new_string[i] == "T":
+            new_string[i] = "A"
+        elif new_string[i] == "A":
+            new_string[i] = "T"
+        elif new_string[i] == "G":
+            new_string[i] = "C"
+        elif new_string[i] == "C":
+            new_string[i] = "G"
+            i += 1
+
+    new_string1 = list_to_string(new_string)
+    return new_string1
 
 def reverse_string(string):
     rstr1 = ""
@@ -18,23 +35,6 @@ def reverse_string(string):
 
 def list_to_string(s):
     str1 = ""
-    for ele in s:
-        str1 += ele
+    for ltr in s:
+        str1 += ltr
     return str1
-
-def get_dna_complement(dna):
-    reverse = reverse_string(dna)
-    new_reverse = list(reverse)
-    for i in range(10):
-        if new_reverse[i] == "T":
-            new_reverse[i] = "A"
-        elif new_reverse[i] == "A":
-            new_reverse[i] = "T"
-        elif new_reverse[i] == "G":
-            new_reverse[i] = "C"
-        elif new_reverse[i] == "C":
-            new_reverse[i] = "G"
-            i += 1
-
-    new_reverse1 = list_to_string(new_reverse)
-    return new_reverse1
