67166b6e567ce6bb3d5dbaf05c622cf182a65b1d
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-04-16 17:27:28 -0500

    HW 7 4/16

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,4 +1,21 @@
-#
+# Homework 7
+
+def add_inventory(type_dictionary, widget_name, quantity):
+    if widget_name not in type_dictionary:
+        type_dictionary[widget_name] = quantity
+    else:
+        type_dictionary[widget_name] += quantity
+    return type_dictionary
+
+def remove_inventory_widget(type_dictionary, widget_name):
+    if widget_name in type_dictionary:
+        del type_dictionary[widget_name]
+        print(widget_name,': Record DELETED')
+    else:
+        print('ITEM NOT FOUND')
+    return type_dictionary
+
+# Homework 6
 def get_p_distance(list1, list2):
     index = 0
     diff = 0
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,9 +1,37 @@
 import unittest #
 
-from src.homework.i_dictionaries_sets.dictionary import get_p_distance, get_p_distance_matrix
+from src.homework.i_dictionaries_sets.dictionary import add_inventory, get_p_distance, get_p_distance_matrix, remove_inventory_widget
 
 class Test_config(unittest.TestCase):
+    
+# HW 7
+    def test_add_inventory(self):
+        inventory_dictionary = {}
+        widget_name = 'widget1'
+        quantity = 10
+        expected_dictionary = {widget_name : quantity}
+        self.assertEqual(expected_dictionary, add_inventory(inventory_dictionary, widget_name, quantity))
 
+        inventory_dictionary = {'widget1' : quantity}
+        quantity = 25
+        expected_dictionary = {'widget1' : 35}
+        self.assertEqual(expected_dictionary, add_inventory(inventory_dictionary, widget_name, quantity))
+
+        inventory_dictionary = {'widget1' : 35}
+        quantity = -10
+        expected_dictionary = {'widget1' : 25}
+        self.assertEqual(expected_dictionary, add_inventory(inventory_dictionary, widget_name, quantity))
+
+    def test_remove_inventory_widget(self):
+        inventory_dictionary = {}
+        widget_name = 'widget1'
+        inventory_dictionary['widget1'] = 11
+        inventory_dictionary['widget2'] = 24
+        expected_dictionary = {'widget2' : 24}
+        self.assertEqual(1, len(expected_dictionary))
+        self.assertEqual(expected_dictionary, remove_inventory_widget(inventory_dictionary, widget_name))
+
+# HW 6
     def test_get_p_distance(self):
         list1 = ['T','T','T','C','C','A','T','T','T','A']
         list2 = ['G','A','T','T','C','A','T','T','T','C']
