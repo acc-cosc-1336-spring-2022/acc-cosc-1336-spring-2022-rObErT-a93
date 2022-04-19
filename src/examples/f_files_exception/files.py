aed5f5991dbb7b0c931e6c32838e14861a246ac6
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-04-19 15:52:29 -0500

    Lecture 4/12 Video

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -1,1 +1,23 @@
-#
+# Lecture 4/12
+def open_file(file_name):
+    try:
+        # Work with the file.
+        file_handle = open(file_name, 'r')
+        print('File opened.')
+        total = 0
+
+        for line in file_handle:
+            amount = float(line)
+            total += amount
+        
+        print('Total: ', total)
+        # Close the file.
+    except FileNotFoundError:
+        print('File:', file_name ,'does not exist.')
+
+    except ValueError as err:
+        print(err)
+        #print('File contains invalid data.')
+
+    else:
+        print('No errors occurred.')
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,39 +1,58 @@
-#main program
-file_name = 'Programming.txt'
+# Lecture 4/12
+# Main Program
+import files
 
-# creates the file
-# programming_file = open(file_name, 'w') # writes to file
-programming_file = open(file_name, 'a') # appends to file
+def main():
+    #files.open_file('some_file.txt')
+    #files.open_file('Programming.txt')
+    files.open_file('sales_data.txt')
 
-# uses the file
-# programming_file.write('\tProgramming Languages\n')
-# programming_file.write('C#\n')
-# programming_file.write('Python\n')
-# programming_file.write('Java\n')
-    # appending to file with line 6
-programming_file.write('C\n')
-programming_file.write('Lisp\n')
-programming_file.write('Ada\n')
+    # try:
+    #     num = float(input('Enter payroll hours: '))
+    # except ValueError:
+    #     print('Invalid number entered...')
 
-# close the file
-programming_file.close()
+main()
 
-# open file for reading
-programming_file = open(file_name, 'r')
 
-# uses the file
-for line in programming_file:
-    print(line.rstrip('\n'))
-#programming_file_contents = programming_file.read() 
-# programming_file_contents = programming_file.readline() # returns individual line from file
-# programming_file_contents += programming_file.readline() # += returns another line from file
-# programming_file_contents += programming_file.readline()
-# programming_file_contents += programming_file.readline()
-# programming_file_contents += programming_file.readline()
-# programming_file_contents += programming_file.readline()
-# programming_file_contents += programming_file.readline()
 
-programming_file.close()    # closes the file
 
-# prints the content
-#print(programming_file_contents)
+# #main program
+# file_name = 'Programming.txt'
+
+# # creates the file
+# # programming_file = open(file_name, 'w') # writes to file
+# programming_file = open(file_name, 'a') # appends to file
+
+# # uses the file
+# # programming_file.write('\tProgramming Languages\n')
+# # programming_file.write('C#\n')
+# # programming_file.write('Python\n')
+# # programming_file.write('Java\n')
+#     # appending to file with line 6
+# programming_file.write('C\n')
+# programming_file.write('Lisp\n')
+# programming_file.write('Ada\n')
+
+# # close the file
+# programming_file.close()
+
+# # open file for reading
+# programming_file = open(file_name, 'r')
+
+# # uses the file
+# for line in programming_file:
+#     print(line.rstrip('\n'))
+# #programming_file_contents = programming_file.read() 
+# # programming_file_contents = programming_file.readline() # returns individual line from file
+# # programming_file_contents += programming_file.readline() # += returns another line from file
+# # programming_file_contents += programming_file.readline()
+# # programming_file_contents += programming_file.readline()
+# # programming_file_contents += programming_file.readline()
+# # programming_file_contents += programming_file.readline()
+# # programming_file_contents += programming_file.readline()
+
+# programming_file.close()    # closes the file
+
+# # prints the content
+# #print(programming_file_contents)
