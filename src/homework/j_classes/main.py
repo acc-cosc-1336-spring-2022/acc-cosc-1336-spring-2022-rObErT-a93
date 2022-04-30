dfcbe42ce835b65db15928f89a37f32b599495ba
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-04-30 15:32:51 -0500

    HW 9

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,15 @@
-#
+#HW 9
+import class_a
+
+def main():
+    roll_dice = class_a.Die()
+    user_choice = 'n'
+    user_choice = input('Would you like to roll the dice? (y or n): ')
+    while user_choice.lower() == 'y':
+        roll_dice.roll()
+        print(roll_dice)
+        user_choice = input('Would you like to roll again?: ')
+    while user_choice.lower() == 'n':
+        print('Thanks for rolling...')
+        quit()
+main()
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,33 @@
-#
+import unittest
+from src.homework.j_classes import class_a
+
+class Test_Config(unittest.TestCase):
+
+    def test_roll_value(self):
+        die = class_a.Die()
+        die.roll()
+        if die.get_roll_value() in range(0,7):
+            rolled = True
+        else:
+            rolled = False
+        self.assertEqual(True,rolled)
+
+    def test_first_roll(self):
+        die = class_a.Die()
+        die.roll()
+        self.assertEqual(True, die.get_roll_value() in range(0,7))
+
+    def test_second_roll(self):
+        die = class_a.Die()
+        die.roll()
+        self.assertEqual(True, die.get_roll_value() in range(0,7))
+
+    def test_third_roll(self):
+        die = class_a.Die()
+        die.roll()
+        self.assertEqual(True, die.get_roll_value() in range(0,7))
+
+    def test_roll_outside_range(self):
+        die = class_a.Die()
+        die.roll()
+        self.assertEqual(False, die.get_roll_value() in range(7,13))
